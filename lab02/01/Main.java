6923ecf9b2d8d30845bf334590e75602510f770b
balencec <ttinstamm@gmail.com>
2026-09-23 02:10:56 +0800

    Add files via upload

---
+++ lab02/01/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int class = 1;
+        int public = 1;
+        int static = 1;
+        int void = 1;
+        int if = 1;
+    }
+}
---
+++ lab02/01/main.py
@@ -0,0 +1,5 @@
+def = 1
+if = 1
+while = 1
+import = 1
+class = 1
